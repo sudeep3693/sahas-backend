c123d0742df6ea3c2c0ea828281d6dd764fa6366
sudeep3693 <sudeepsubedi72@gmail.com>
2025-06-29 23:11:32 +0545

    database updated

--- src/MiddleWare/DatabaseConnection.js
+++ src/MiddleWare/DatabaseConnection.js
@@ -1,14 +1,14 @@
 import mongoose from 'mongoose';
-const DBConnect = (req, res, next) =>{
+const DBConnect = (req, res, next) => {
 
-    mongoose.connect('mongodb+srv://sudeepsubedi72:wdf4A8ypJKVMxunU@sahascooperative.eyybn0u.mongodb.net/?retryWrites=true&w=majority&appName=SahasCooperative', {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    })
+  mongoose.connect('mongodb+srv://sudeepsubedi72:wdf4A8ypJKVMxunU@sahascooperative.eyybn0u.mongodb.net/SahasCooperative?retryWrites=true&w=majority&appName=SahasCooperative', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
     .then(() => console.log('Database connected successfully'))
     .catch((err) => console.error('Error while connecting to database:', err));
-    
-    next();
+
+  next();
 }
 // 'mongodb://localhost:27017/Sahas'
 //process.env.MONGO_URI
