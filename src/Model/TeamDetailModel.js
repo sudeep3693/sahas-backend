212a2c7529afb649442bb37b89a8cae8f3988e3d
sudeep3693 <sudeepsubedi72@gmail.com>
2025-08-20 05:53:13 +0545

    fix: order position created in team details

--- src/Model/TeamDetailModel.js
+++ src/Model/TeamDetailModel.js
@@ -4,7 +4,8 @@ const teamDetailSchema = new mongoose.Schema({
   name: { type: String, required: true },
   position: { type: String, required: true },
   category: { type: String, required: true },
-  imageName: { type: String, required: true }
+  imageName: { type: String, required: true },
+  positionOrder: { type: Number, default: 0 },
 }, {
   timestamps: true
 });
